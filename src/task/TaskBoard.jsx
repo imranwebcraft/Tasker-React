1737c6439bfb1af50cc70efc9025ff8309868efd
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-16 23:12:42 +0600

    Delete all task functionality added

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -39,6 +39,11 @@ const TaskBoard = () => {
 		setTasks(tasks.filter((task) => task.id !== taskId));
 	};
 
+	const handleDeleteAllClick = () => {
+		tasks.length = 0;
+		setTasks([...tasks]);
+	};
+
 	const handleEditTask = (task) => {
 		setShoeAllModal(true);
 		setTaskToUpdate(task);
@@ -61,7 +66,10 @@ const TaskBoard = () => {
 			<div className="container">
 				<SearchTask />
 				<div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-					<TaskAction onAddClick={() => setShoeAllModal(!showAllModal)} />
+					<TaskAction
+						onAddClick={() => setShoeAllModal(!showAllModal)}
+						onDeleteOnClick={handleDeleteAllClick}
+					/>
 					<TaskList
 						tasks={tasks}
 						onEdit={handleEditTask}
