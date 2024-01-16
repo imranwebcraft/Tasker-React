fd5a4086b87064de3153df498ab4f444016bd33b
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-16 18:05:53 +0600

    Add task modal functionality added with modal close

--- src/task/AddTaskModal.jsx
+++ src/task/AddTaskModal.jsx
@@ -1,7 +1,37 @@
-const AddTaskModal = () => {
+import { useState } from "react";
+
+const AddTaskModal = ({ onSave, onClose }) => {
+	const [addATask, setAddATask] = useState({
+		id: crypto.randomUUID(),
+		title: "",
+		description: "",
+		tags: [],
+		priority: "",
+		isFavorite: false,
+	});
+
+	const handleChange = (e) => {
+		let name = e.target.name;
+		let value = e.target.value;
+
+		if (name === "tags") {
+			value = value.split(",");
+		}
+
+		setAddATask({
+			...addATask,
+			[name]: value,
+		});
+	};
+
+	const handleTaskAdd = (e) => {
+		e.preventDefault();
+		onSave(addATask);
+	};
+
 	return (
 		<>
-			<div className=" z-10 absolute bg-black w-full h-screen top-0 left-0 bg-opacity-60"></div>
+			<div className=" z-10 absolute bg-black w-full h-screen inset-0 bg-opacity-60"></div>
 			<form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-[0%] left-1/3">
 				<h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
 					Add New Task
@@ -15,6 +45,8 @@ const AddTaskModal = () => {
 							type="text"
 							name="title"
 							id="title"
+							value={addATask.title}
+							onChange={handleChange}
 							required
 						/>
 					</div>
@@ -25,6 +57,8 @@ const AddTaskModal = () => {
 							type="text"
 							name="description"
 							id="description"
+							value={addATask.description}
+							onChange={handleChange}
 							required
 						></textarea>
 					</div>
@@ -37,6 +71,8 @@ const AddTaskModal = () => {
 								type="text"
 								name="tags"
 								id="tags"
+								value={addATask.tags}
+								onChange={handleChange}
 								required
 							/>
 						</div>
@@ -46,12 +82,14 @@ const AddTaskModal = () => {
 								className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
 								name="priority"
 								id="priority"
+								value={addATask.priority}
+								onChange={handleChange}
 								required
 							>
 								<option value="">Select Priority</option>
-								<option value="low">Low</option>
-								<option value="medium">Medium</option>
-								<option value="high">High</option>
+								<option value="Low">Low</option>
+								<option value="Medium">Medium</option>
+								<option value="High">High</option>
 							</select>
 						</div>
 					</div>
@@ -59,10 +97,20 @@ const AddTaskModal = () => {
 
 				<div className="mt-16 flex justify-center lg:mt-20">
 					<button
+						onClick={handleTaskAdd}
 						type="submit"
 						className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
 					>
 						Create new Task
+					</button>
+				</div>
+				<div className="mt-16 flex justify-center lg:mt-20">
+					<button
+						onClick={onClose}
+						type="button"
+						className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+					>
+						Close
 					</button>
 				</div>
 			</form>
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -18,9 +18,19 @@ const TaskBoard = () => {
 	const [tasks, setTasks] = useState([defaultTask]);
 	const [showAllModal, setShoeAllModal] = useState(true);
 
+	const handleAddTask = (newTask) => {
+		setTasks([...tasks, newTask]);
+		setShoeAllModal(false);
+	};
+
 	return (
 		<section className="mb-20" id="tasks">
-			{showAllModal && <AddTaskModal />}
+			{showAllModal && (
+				<AddTaskModal
+					onSave={handleAddTask}
+					onClose={() => setShoeAllModal(false)}
+				/>
+			)}
 			<div className="container">
 				<SearchTask />
 				<div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
